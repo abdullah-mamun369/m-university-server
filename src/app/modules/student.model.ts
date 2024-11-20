0e063426df15338624b7b7daaa2199ee81f4354e
abdullah-mamun369 <maamamun369@gmail.com>
2024-11-20 20:14:02 +0600

    data read is successfully done

---
+++ src/app/modules/student.model.ts
@@ -0,0 +1,52 @@
+import { Schema, model } from 'mongoose';
+import {
+  Guardian,
+  LocalGuardian,
+  Student,
+  UserName,
+} from './student/student.interface';
+
+const userNameSchema = new Schema<UserName>({
+  firstname: { type: String, required: true },
+  middlename: { type: String },
+  lastname: { type: String, required: true },
+});
+
+const guardianSchema = new Schema<Guardian>({
+  fatherName: { type: String, required: true },
+  fatherOccupation: { type: String, required: true },
+  fatherContactNo: { type: String, required: true },
+  motherName: { type: String, required: true },
+  motherOccupation: { type: String, required: true },
+  motherContactNo: { type: String, required: true },
+});
+
+const localGuardianSchema = new Schema<LocalGuardian>({
+  name: { type: String, required: true },
+  occupation: { type: String, required: true },
+  contactNo: { type: String, required: true },
+  address: { type: String, required: true },
+});
+
+const studentSchema = new Schema<Student>({
+  id: { type: String },
+  name: userNameSchema,
+  gender: { type: String, enum: ['male', 'female'], required: true },
+  dateOfBirth: { type: String },
+  email: { type: String, required: true },
+  contactNo: { type: String, required: true },
+  emergyncyContactNo: { type: String, required: true },
+  bloodGroup: {
+    type: String,
+    enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+    required: true,
+  },
+  presentAddress: { type: String, required: true },
+  permanentAddress: { type: String, required: true },
+  guardian: guardianSchema,
+  localGuardian: localGuardianSchema,
+  profileImg: { type: String },
+  isActive: { type: String, enum: ['active', 'blocked'], required: true },
+});
+
+export const StudentModel = model<Student>('Student', studentSchema);
---
+++ src/app/modules/student/student.controller.ts
@@ -0,0 +1,53 @@
+import { Request, Response } from 'express';
+import { StudentServices } from './student.service';
+
+const createStudent = async (req: Request, res: Response) => {
+  try {
+    const { student: studentData } = req.body;
+    const result = await StudentServices.createStudentIntoDB(studentData);
+
+    res.status(200).json({
+      success: true,
+      message: 'Student is created succesfully',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getAllStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentsFromBD();
+
+    res.status(200).json({
+      success: true,
+      message: 'Students are retrived succesfully',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getSingleStudent = async (req: Request, res: Response) => {
+  try {
+    const { studentId } = req.params;
+
+    const result = await StudentServices.getSingleStudentFromBD(studentId);
+
+    res.status(200).json({
+      success: true,
+      message: 'Student is retrived succesfully',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const StudentControllers = {
+  createStudent,
+  getAllStudents,
+  getSingleStudent,
+};
