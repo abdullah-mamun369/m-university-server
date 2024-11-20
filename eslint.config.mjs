4f5b362f1e3af282064e60bf756d1b9f73ddf7e3
abdullah-mamun369 <maamamun369@gmail.com>
2024-11-20 16:32:52 +0600

    Project setup is done with mongoose,typescript, cors, expresss,eslint and prettier with typescript

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+
+module.exports = [
+  // Any other config imports go at the top
+  eslintPluginPrettierRecommended,
+];
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.browser } },
+  {
+    rules: {
+      eqeqeq: 'off',
+      'no-unused-vars': 'error',
+      'prefer-const': ['error', { ignoreReadBeforeAssign: true }],
+      'no-unused-expressions': 'error',
+      'no-console': 'warn',
+      'no-undef': 'error',
+    },
+  },
+  {
+    ignores: ['.node_modules/*', '.dist/*'],
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
