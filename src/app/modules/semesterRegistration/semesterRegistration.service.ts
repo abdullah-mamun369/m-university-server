d2ebefe81f2b2bac0684fc0c34b0368b75f983fe
abdullah-mamun369 <maamamun369@gmail.com>
2024-12-07 17:15:42 +0600

    Create Semester Registration is done

---
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -0,0 +1,48 @@
+import httpStatus from 'http-status';
+import AppError from '../../errors/AppError';
+import { AcademicSemester } from '../academicSemester/academicSemester.model';
+import { TSemesterRegistration } from './semesterRegistration.interface';
+import { SemesterRegistration } from './semesterRegistration.model';
+
+const createSemesterRegistrationIntoDB = async (
+  payload: TSemesterRegistration,
+) => {
+  /**
+   
+   * Step2: Check if the semester is exist
+   * Step3: Check if the semester is already registered!
+   * Step4: Create the semester registration
+   */
+
+  const academicSemester = payload?.academicSemester;
+
+  // check if the semester is exist
+  const isAcademicSemesterExists =
+    await AcademicSemester.findById(academicSemester);
+
+  if (!isAcademicSemesterExists) {
+    throw new AppError(
+      httpStatus.NOT_FOUND,
+      'This academic semester not found !',
+    );
+  }
+
+  // check if the semester is already registered!
+  const isSemesterRegistrationExists = await SemesterRegistration.findOne({
+    academicSemester,
+  });
+
+  if (isSemesterRegistrationExists) {
+    throw new AppError(
+      httpStatus.CONFLICT,
+      'This semester is already registered!',
+    );
+  }
+
+  const result = await SemesterRegistration.create(payload);
+  return result;
+};
+
+export const SemesterRegistrationService = {
+  createSemesterRegistrationIntoDB,
+};
