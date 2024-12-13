775e73dc86519a877563da3be35e71a67e3cfa6b
abdullah-mamun369 <maamamun369@gmail.com>
2024-12-13 22:36:01 +0600

    Course authorization is done

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import cookieParser from 'cookie-parser';
 import express, {
   Application,
   Request,
@@ -13,7 +14,8 @@ const app: Application = express();
 
 //parsers
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser());
+app.use(cors({ origin: ['http://localhost:5173'] }));
 
 // application routes
 app.use('/api/v1', router);
