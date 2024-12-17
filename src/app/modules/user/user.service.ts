78d0e7f07ab75babb6fe718dd4d6deed86d99e0b
abdullah-mamun369 <maamamun369@gmail.com>
2024-12-17 10:11:03 +0600

    enrolled course interface, model, validation and constant is done

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import httpStatus from 'http-status';
 import mongoose from 'mongoose';
